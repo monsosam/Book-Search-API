25ebe3f10c674ab9730c078c02f7c00b3703f0c6
monsosam <monsonsamuel5@gmail.com>
2024-02-17 19:38:03 -0700

    commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,82 +3,82 @@ const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
-    Query: {
-      me: async (_, _, context) => {
-        if (!context.user) {
-          throw new AuthenticationError('Not authenticated');
+  Query: {
+    me: async (_, __, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    login: async (_, { email, password }) => {
+      try {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('Incorrect email or password');
         }
-        try {
-          const user = await User.findOne({ _id: context.user._id }).populate("savedBooks");
-          return user;
-        } catch (error) {
-          throw new Error('Error fetching user');
+  
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect password');
         }
-      },
+        
+        const token = signToken(user);
+        return { token, user };
+
+      } catch (error) {
+        throw new Error('Error during login');
+      }
     },
 
-    Mutation: {
-      login: async (_, { email, password }) => {
-        try {
-          const user = await User.findOne({ email });
+    addUser: async (_, { username, email, password }) => {
+      try {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
   
-          if (!user || !(await user.isCorrectPassword(password))) {
-            throw new AuthenticationError('Incorrect email or password');
-          }
-  
-          const token = signToken(user);
-  
-          return { token, user };
-        } catch (error) {
-          throw new Error('Error during login');
-        }
-      },
-
-      addUser: async (_, { username, email, password }) => {
-        try {
-          const user = await User.create({ username, email, password });
-          const token = signToken(user);
-  
-          return { token, user };
-        } catch (error) {
-          throw new Error('Error during user creation');
-        }
-      },
-
-      saveBook: async (_, { input }, context) => {
-        if (!context.user) {
-          throw new AuthenticationError('Not authenticated');
-        }
-        try {
-          const updatedUser = await User.findByIdAndUpdate(
-            { _id: context.user._id },
-            { $push: { savedBooks: input } },
-            { new: true, runValidators: true }
-          );
-          return updatedUser;
-
-        } catch (error) {
-          throw new Error('Error saving book');
-        }
-      },
-
-      removeBook: async (_, { input }, context) => {
-        if (!context.user) {
-          throw new AuthenticationError('Not authenticated');
-        }
-        try {
-          const updatedUser = await User.findByIdAndUpdate(
-            { _id: context.user._id },
-            { $pull: { savedBooks: { input } } },
-            { new: true }
-          );
-          return updatedUser;
-
-        } catch (error) {
-          throw new Error('Error removing book');
-        }
-      },
+        return { token, user };
+      } catch (error) {
+        throw new Error('Error during user creation');
+      }
     },
-  };
+
+    saveBook: async (_, { input }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('Not authenticated');
+      }
+      try {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+
+      } catch (error) {
+        throw new Error('Error saving book');
+      }
+    },
+
+    removeBook: async (_, { input }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('Not authenticated');
+      }
+      try {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { input } } },
+          { new: true }
+        );          
+        return updatedUser;
+
+      } catch (error) {
+        throw new Error('Error removing book');
+      }
+    },
+  },
+};
   
   module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
